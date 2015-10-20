d57cde0a27defc2132abe9cbd10a694200693ce9
David Robertson <david.m.robertson1@gmail.com>
2015-10-20 11:17:50 +0100

    Client name defaults to PC name

--- metaox/client.py
+++ metaox/client.py
@@ -1,14 +1,23 @@
 import asyncio
 import logging
+from socket import gethostbyaddr
+import re
 
 from .protocol import transmit_state
 
+extract_host_name = re.compile(r'(\w+)(?:\.campus)?\.ncl\.ac\.uk')
+
 class Client:
 	def __init__(self, id, socket):
+		#todo allow the client to provide their own name upon connection (cookie)
 		self.id = id
 		self.socket = socket
-		self.name = "client {}".format(id)
-	
+		dns_name = gethostbyaddr(socket.remote_address[0])[0].lower()
+		match = extract_host_name.match(dns_name)
+		if match:
+			self.name = match.groups()[0]
+		else:
+			self.name = "client {}".format(id)
 	def __str__(self):
 		return 'Client #{} ({})'.format(
 		  self.id, self.name) 
